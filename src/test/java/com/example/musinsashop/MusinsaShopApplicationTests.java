964d5a2d3e8b39f10e776c1dbde10068b48511d7
younghun <younghun@paycurx.co.kr>
2022-08-17 18:30:24 +0900

    feat (feature) : init project
    feat (feature) : init project
    - 프로젝트 생성 및 추가

---
+++ src/test/java/com/example/musinsashop/MusinsaShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.musinsashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusinsaShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
