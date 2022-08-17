14bb328727f33dde24f29d09f01ac6cf75129071
younghun <younghun@paycurx.co.kr>
2022-08-17 22:04:55 +0900

    test (when adding missing tests) : Category Test 추가
    test (when adding missing tests) : Category Test 추가
    - categoryAddDto를 이용하여 category 객체를 생성 테스트

---
+++ src/test/java/com/example/musinsashop/domain/CategoryTest.java
@@ -0,0 +1,27 @@
+package com.example.musinsashop.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.example.musinsashop.dto.CategoryAddDto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CategoryTest {
+
+    @DisplayName("categoryAddDto를 이용하여 category 객체를 생성 테스트")
+    @Test
+    void createCategory() {
+        //given
+        CategoryAddDto categoryAddDto = CategoryAddDto.builder()
+                .name("상의")
+                .status(DataStatus.DEFAULT)
+                .build();
+
+        //when
+        Category category = categoryAddDto.toEntity();
+
+        //then
+        assertThat(category.getName()).isEqualTo(categoryAddDto.getName());
+    }
+
+}
